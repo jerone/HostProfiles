e25e655849267de30e58c0b908f6e5f8ccf0407c
jerone <jeronevw@hotmail.com>
2013-10-15 23:31:49 +0200

    Updated version

--- HostProfiles/FormAbout.Designer.cs
+++ HostProfiles/FormAbout.Designer.cs
@@ -70,7 +70,7 @@
 			this.label2.Name = "label2";
 			this.label2.Size = new System.Drawing.Size(95, 17);
 			this.label2.TabIndex = 2;
-			this.label2.Text = "Version: 2.0";
+			this.label2.Text = "Version: 2.1";
 			// 
 			// label3
 			// 
--- HostProfiles/Properties/AssemblyInfo.cs
+++ HostProfiles/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("HostProfiles")]
-[assembly: AssemblyDescription("http://www.shootsoft.net")]
+[assembly: AssemblyDescription("https://github.com/jerone/HostProfiles")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Shoot Sun Studio")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("HostProfiles")]
-[assembly: AssemblyCopyright("Copyright © Shoot Sun Studio 2011")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
